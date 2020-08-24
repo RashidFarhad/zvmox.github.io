090d4e04ddadd6f030e400276a4289d1727a5b9d
Zvmox <62778188+Zvmox@users.noreply.github.com>
2020-08-24 19:54:50 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ 
+    var typed = new Typed(".typed", {
+        strings: ["Reverse Engineer", "Web Developer", "Lecture"],
+        typeSpeed:70,
+        loop: true,
+        startDelay: 0,
+        showCursor:false,
+    });
+
+
+});
